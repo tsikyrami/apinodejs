3d5f3a32341f8ee18b966a516a02a9d55fcb0de9
tsiky <TsikyAnjaraNomena@gmail.com>
2023-10-24 11:34:09 +0300

    cors

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(api_prefix,api_router);
+app.use((api_prefix,api_router,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.listen(port, () => {
     console.log("The server listens on the port ".concat(port));
